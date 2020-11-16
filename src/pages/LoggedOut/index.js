a42465b621301b8207a9c291e6b1554aeb6d8159
Andre Garrido Damaceno <Adamacenog@users.noreply.github.com>
2020-11-15 21:49:07 -0300

    #16 logged out screen design improvement

--- src/pages/LoggedOut/index.js
+++ src/pages/LoggedOut/index.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import Button from '../../components/Button';
+import { Container, Button, TextField, HeaderField} from "./styles.js"
 import { useRoute, useNavigation } from '@react-navigation/native';
 
 const LoggedOut = () => {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>
-        Você não pode realizar esta ação sem possuir um cadasto. 
-      </Text>
+    <Container>
+      <HeaderField>
+        Ops!
+      </HeaderField>
+
+      <TextField>
+        Você não pode realizar esta ação sem possuir um cadasto.
+      </TextField>
+
       <Button onPress={() => {
         navigation.navigate('SignUp')
       }}>
         FAZER CADASTRO
       </Button>
+
+      <TextField>
+        Já possui cadastro?
+      </TextField>
+
       <Button onPress={() => {
         navigation.navigate('SignIn')
       }}>
         FAZER LOGIN
       </Button>
-    </View>
+    </Container>
   )
 }
 
---
+++ src/pages/LoggedOut/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import ButtonStyle from '../../components/Button';
+
+export const Container = styled.View`
+    flex: 1;
+`;
+
+export const Button = styled(ButtonStyle)`
+    text-align: center;
+    font-size: 12px;
+    margin: 8px auto;
+    width: 232px; 
+    height: 40px;
+    font-family: 'Roboto-Regular';
+`;
+
+export const TextField = styled.Text`
+  color: #757575;
+  font-size: 14px;
+  font-family: 'Roboto-Regular';
+  text-align: center;
+  margin: 16px 0 16px 0;
+`;
+
+export const HeaderField = styled.Text`
+  color: #88c9bf;
+  font-size: 64px;
+  font-family: 'Courgette-Regular';
+  text-align: center;
+  margin: 16px 0 16px 0;
+`;
